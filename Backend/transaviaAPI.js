8c1403783d2d37f416e97cb80c1168c41b6e1063
Dorian Grolaux <dgrolaux@nl.ibm.com>
2015-10-30 23:21:26 +0100

    Changed the way of doing module.exports & corrected a small annoying typo that broke everything

--- Backend/transaviaAPI.js
+++ Backend/transaviaAPI.js
@@ -1,55 +1,60 @@
-var transavia = function () {
+//var transavia = function () {
 
-  //quick access to url resources
-  this.resources = {
-    origin: '&orgin=', //3 letter airport id
-    destination: '&destination=', //3 letter airport id
-    origindeparturedate: '&origindeparturedate=', //yyyyMM(dd)
-    origindeparturetime: '&origindeparturetime=', //timerange (0000-2359)
-    origindeparturedayofweek: '&origindeparturedayofweek=', //yyyyMM(dd)
-    adults: '&adults=', //required field
-    price: '&price=', //price range (50-200)
-    grouppricing: '&groupPricing=', //true = total price for all people, false = price/person
-    directflight: '&DirectFlight=', //pretty much self explanatory
-    orderby: '&orderby='
-      /*
-        Comma-separated list of fields on which the result must be ordered.
-        Allowed values: 'Origin', 'Destination', 'OriginDepartureDate', 
-        'DestinationArrivalDate', 'Price'. 
-        For return flights also the values 'DestinationDepartureDate' and 
-        'OriginArrivalDate' can be used. 
-        (default = OriginDepartureDate, Origin, Destination)
-      */
-  };
+//quick access to url resources
+var resources = {
+  origin: '&origin=', //3 letter airport id
+  destination: '&destination=', //3 letter airport id
+  origindeparturedate: '&origindeparturedate=', //yyyyMM(dd)
+  origindeparturetime: '&origindeparturetime=', //timerange (0000-2359)
+  origindeparturedayofweek: '&origindeparturedayofweek=', //yyyyMM(dd)
+  adults: '&adults=', //required field
+  price: '&price=', //price range (50-200)
+  grouppricing: '&groupPricing=', //true = total price for all people, false = price/person
+  directflight: '&DirectFlight=', //pretty much self explanatory
+  orderby: '&orderby='
+    /*
+      Comma-separated list of fields on which the result must be ordered.
+      Allowed values: 'Origin', 'Destination', 'OriginDepartureDate', 
+      'DestinationArrivalDate', 'Price'. 
+      For return flights also the values 'DestinationDepartureDate' and 
+      'OriginArrivalDate' can be used. 
+      (default = OriginDepartureDate, Origin, Destination)
+    */
+};
 
-  /**
-   * pass the values you want to search and it will return a url link you can GET
-   * @param {Object} params A collection of parameters and their values
-   *                        {
-   *                        param [
-   *                        val, val, val
-   *                        ],
-   *                        param [
-   *                          val, val, val
-   *                          ]
-   */
-  this.buildUrl = function (params) {
-    var result = '';
+/**
+ * pass the values you want to search and it will return a url link you can GET
+ * @param {Object} params A collection of parameters and their values
+ *                        {
+ *                        param [
+ *                        val, val, val
+ *                        ],
+ *                        param [
+ *                          val, val, val
+ *                          ]
+ */
+var buildUrl = function (params) {
+  console.log('function');
+  var result = '';
 
-    for (var index in param) {
-      if (params.hasOwnProperty(index)) {
-        var param = params[index];
+  for (var index in params) {
+    if (params.hasOwnProperty(index)) {
+      var param = params[index];
+      
+      console.log(param);
 
-        result += resources[index];
+      result += resources[index];
 
-        param.forEach(function (value) {
-          result += value + ',';
-        });
-      }
+      param.forEach(function (value) {
+        result += value + ',';
+      });
+      
+      result = result.substr(0, result.length - 1);
     }
-
-    return result;
   }
-}
 
-module.exports = transavia;
+  return result;
+};
+
+module.exports.resources = resources;
+module.exports.buildUrl = buildUrl;
